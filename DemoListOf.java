299ffa9da8c3b438ec70182489c38bd99aee0f12
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-08 20:09:46 +0300

    Add files via upload

---
+++ DemoListOf.java
@@ -0,0 +1,11 @@
+package newfeatures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoListOf {
+    public static void main(String[] args) {
+        var list = List.of(10,20,30);
+        System.out.println(list);
+    }
+}
